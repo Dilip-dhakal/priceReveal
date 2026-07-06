0c063d3f6c6238b3a04a2ac51efbd0e8915c95e0
Dilip17-cyber <lamjung0088@gmail.com>
2026-07-06 18:57:03 +0545

    feat:Auth Controller

--- backend/src/features/auth/auth.controller.ts
+++ backend/src/features/auth/auth.controller.ts
@@ -1,5 +1,16 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import { registerService } from "./auth.service.js";
 
-export const registerUser=async (req:Request,res:Response)=>{
-    
+
+export const registerController = async (
+    req: Request,
+    res: Response
+) => {
+    const { email, password } = req.body;
+    const user = await registerService(email, password);
+    return res.status(201).json({
+    success: true,
+    message: "User registered successfully",
+    data: user,
+});
 }
