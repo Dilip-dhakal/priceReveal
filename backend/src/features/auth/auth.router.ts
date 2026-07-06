2de7e1e107f924d898a6f855b1ec0314bcc13a5c
Dilip17-cyber <lamjung0088@gmail.com>
2026-07-06 18:57:06 +0545

    feat:Auth Controller

--- backend/src/features/auth/auth.router.ts
+++ backend/src/features/auth/auth.router.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import registerSchema from './auth.schema.js'
+import { registerController } from './auth.controller.js'
+import {  validateRequest } from '../../middlewares/validate.middleware.js'
 
 const router=express.Router()
 
-// router.post("/register",validateRequest(registerSchema),)
+router.post("/register",validateRequest(registerSchema),registerController)
 
 export default router
