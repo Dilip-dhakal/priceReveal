86e7b7cab1988686947762a63867b9f95ea5fe18
Dilip17-cyber <lamjung0088@gmail.com>
2026-07-06 18:51:19 +0545

    feat:Added auth service

---
+++ backend/src/middlewares/validate.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+import { ErrorHandler } from "../errors/error.handler.js";
+
+export const validate = (schema: z.ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse({
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    });
+
+    if (!result.success) {
+      return next(new ErrorHandler(400, result.error.message));
+    }
+
+    next();
+  };
+};
